42e5596d9b04dbb883c3ef903a6e614d55150d1b
akhil683 <onkar.palsra@gmail.com>
2024-05-31 14:18:48 +0530

    fixed home sections layout shift in mobile

--- src/components/Home/Activities.jsx
+++ src/components/Home/Activities.jsx
@@ -8,29 +8,31 @@ const Activities = () => {
 
   return (
    <Container>
-      <div className='relative md:min-h-screen font-poppins flex justify-center md:items-center gap-[110px] flex-wrap-reverse'>
+      <div className='relative md:min-h-screen font-poppins flex justify-center md:items-center gap-6 flex-wrap-reverse'>
 
-         <h2 className='absolute md:top-[20%] top-[270px] left-0 md:text-7xl text-4xl font-semibold text-green-600 max-w-[900px] z-20'>Nature's Playground: Adventure Activities</h2>
 
          <div className='md:pt-48 text-lg max-w-[400px]'>
-            <p className='mb-2'><span className='text-4xl font-semibold text-red-500'>D</span>iscover the Great Outdoors fun activites like </p>
-            <ul className='mb-4'>
-               <li>Paragliding</li>
-               <li>Rafting</li>
-               <li>Skiing</li> 
-               <li>Quad Biking</li>
-               <li>Camping</li>
-               <li>Downhill Cycling</li>
+            <p><span className='text-4xl font-semibold text-red-500'>D</span>iscover the Great Outdoors fun activites like </p>
+            <ul className='my-6 px-2 text-base'>
+               <li>- Paragliding</li>
+               <li>- Rafting</li>
+               <li>- Skiing</li> 
+               <li>- Quad Biking</li>
+               <li>- Camping</li>
+               <li>- Downhill Cycling</li>
             </ul>
             <Link>
                <Button color='red'>Explore Activities</Button>
             </Link>
          </div>
 
+         <h2 className='md:absolute md:top-[20%] left-0 md:text-7xl text-4xl font-semibold text-green-600 max-w-[900px] z-20'>Nature's Playground: Adventure Activities</h2>
+
          <div className=' md:h-[500px] h-[250px] md:w-[700px] w-[400px] relative'>
             <img src={activitiesImg} alt="Adventure Activities" className='absolute h-full w-full object-cover' />
          </div>
 
+         
       </div>
    </Container>
   )
--- src/components/Home/Vehicles.jsx
+++ src/components/Home/Vehicles.jsx
@@ -7,17 +7,18 @@ import Button from '../Button'
 const Vehicles = () => {
   return (
     <Container>
-      <div className='relative md:min-h-screen font-poppins flex justify-center md:items-center gap-[110px] flex-wrap'>
+      <div className='relative md:min-h-screen font-poppins flex justify-center md:items-center gap-6 flex-wrap'>
 
-         <h2 className='absolute md:left-[30%] md:top-[20%] top-[270px] md:text-7xl text-4xl font-semibold text-green-600 max-w-[900px] z-20'>Nature's Playground: Enjoy with Bikes!</h2>
 
          <div className=' md:h-[500px] h-[250px] md:w-[700px] w-[400px] relative bg-black'>
             <img src={vehicleImg} alt="Adventure Activities" className='absolute h-full w-full object-cover opacity-80' />
          </div>
 
-         <div className='md:pt-32 text-lg max-w-[400px]'>
-            <p className='mt-2'><span className='text-4xl font-semibold text-red-500'>D</span>iscover the way of Enjoying your life by renting bikes</p>
-            <p className='px-2 my-4'>
+         <h2 className='md:absolute md:left-[30%] md:top-[18%] md:text-7xl text-4xl font-semibold text-green-600 max-w-[900px] z-20'>Nature's Playground: Enjoy with Bikes!</h2>
+
+         <div className='md:pt-48 text-lg max-w-[400px]'>
+            <p><span className='text-4xl font-semibold text-red-500'>D</span>iscover the way of Enjoying your life by renting bikes</p>
+            <p className='px-2 my-4 text-base'>
                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates sapiente reiciendis laboriosam dolorum, quidem repellat dolorem dignissimos laborum commodi qui voluptas reprehenderit illo odit quos quod, corrupti saepe praesentium animi at voluptatem! At debitis, quibusdam non reiciendis.  
             </p>
             <Link>
